7e4e58003f493dbb0d36db639a8122bfa0e6bc25
ruggedlyOptimistic <jason.davis0311@gmail.com>
2024-12-22 23:27:28 -0800

    project built and runnable

---
+++ build/src/CMakeFiles/bankify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bankify.dir/main.cpp.o"
+  "bankify"
+  "bankify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bankify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
